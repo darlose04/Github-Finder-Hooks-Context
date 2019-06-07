54bb271d03b02ed39faa0c1b5ea9e3272c894eb0
Zach Smith <zsmith126028@gmail.com>
2019-06-07 08:30:24 -0500

    change to hooks and useState

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,57 +1,68 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class Search extends Component {
-  state = {
-    text: ""
-  };
+// convert to function component for hooks
+// props are now brought in as arguments
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+  // don't use it like this since we're using hooks
+  // state = {
+  //   text: ""
+  // };
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  };
-  onSubmit = e => {
+  const [text, setText] = useState("");
+
+  // remove references to 'this'
+
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("Please enter something", "light");
+    if (text === "") {
+      setAlert("Please enter something", "light");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      searchUsers(text);
+      setText("");
     }
   };
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  // remove this.setState and just set it to the value we want to set it
+  const onChange = e => {
+    // this.setState({ [e.target.name]: e.target.value });
+    setText(e.target.value);
   };
 
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className='form'>
-          <input
-            type='text'
-            name='text'
-            placeholder='Search Users...'
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type='submit'
-            value='Search'
-            className='btn btn-dark btn-block'
-          />
-        </form>
-        {showClear && (
-          <button className='btn btn-light btn-block' onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+  // this stuff gets brought in as an arg in the arrow function
+  // const { showClear, clearUsers } = this.props;
+
+  // since this is no longer in a Class, we can remove 'this'
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {showClear && (
+        <button className='btn btn-light btn-block' onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
 
 export default Search;

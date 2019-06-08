841925cd2cfe45f1561459d451033d5dba4a0de2
Zach Smith <zsmith126028@gmail.com>
2019-06-07 19:28:06 -0500

    add searchUser

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -18,9 +18,23 @@ const GithubState = props => {
     loading: false
   };
 
-  const [state, dispatch] = userReducer(GithubReducer, initialState);
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   // Search User
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  };
 
   // Get User
 
@@ -29,6 +43,9 @@ const GithubState = props => {
   // Clear Users
 
   // Set Loading
+  const setLoading = () => {
+    dispatch({ type: SET_LOADING });
+  };
 
   return (
     <GithubContext.Provider
@@ -36,7 +53,8 @@ const GithubState = props => {
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}

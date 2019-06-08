071f99055f20654621dd9108e5d8afd5f50a0327
Zach Smith <zsmith126028@gmail.com>
2019-06-07 19:27:27 -0500

    add githubContext in order to access state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
 // convert to function component for hooks
 // props are now brought in as arguments
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+// const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+// searchUsers is longer a prop, it is accessed with the usedContext hook
+const Search = ({ showClear, clearUsers, setAlert }) => {
   // don't use it like this since we're using hooks
   // state = {
   //   text: ""
   // };
+
+  const githubContext = useContext(GithubContext);
 
   const [text, setText] = useState("");
 
@@ -18,7 +23,8 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
     if (text === "") {
       setAlert("Please enter something", "light");
     } else {
-      searchUsers(text);
+      // searchUsers(text);
+      githubContext.searchUsers(text);
       setText("");
     }
   };
@@ -59,7 +65,7 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
+  // searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
